b4efe83f98f2f260988f57f3f383b86771154047
rleviathan5 <corkyn2005@gmail.com>
2025-10-01 17:25:54 +0100

    chnaging code and commiting from visual studio again

--- testProjCs/Program.cs
+++ testProjCs/Program.cs
@@ -14,6 +14,7 @@ namespace testProjCs
             Application.Run(new Form1());
 
             Console.WriteLine("Hello world!");
+            Console.WriteLine("This was added from visual studio");
         }
     }
 }
